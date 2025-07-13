0677d1eb4b03c5115d018b97a3fa97c1a80174a6
ebenezerrahul <ebenezerrahul@gmail.com>
2025-07-13 10:51:17 +0000

    st-anysize : patch applied

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=32:antialias=true:autohint=true";
+static char *font = "JetBrainsMonoNL NF:style=Regular:pixelsize=32";
 static int borderpx = 2;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.5;
+float alpha = 0.9;
 /* Background opacity */
 float alpha_def;
 
